3f0e58236b5384a5d43a24dd1d798e34fb1c5002
Kyle Petryszak <6314611+ProjectInitiative@users.noreply.github.com>
2022-09-17 17:26:38 -0500

    adding new terminal multiplexer

---
+++ install-nerdfont.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+mkdir -p ~/.config/
+cargo install cargo-make
+
+git -C $SCRIPT_DIR submodule update --init --recursive
+cargo install --path $SCRIPT_DIR/submodules/zellij
