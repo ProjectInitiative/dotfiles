1337533733c250a67694e448bb9e1ebf0832bf36
Kyle Petryszak <6314611+ProjectInitiative@users.noreply.github.com>
2024-10-03 22:21:52 -0500

    adding tracing

--- pkgs/flake.nix
+++ pkgs/flake.nix
@@ -1,35 +1,28 @@
 {
   description = "Nixpkgs with custom inputs and overrides";
-
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    # Add other external flakes as inputs here
     helix.url = "github:helix-editor/helix/162028d444b1e56ee39775460adb65e4f957bc3f";
-    # helix.url = "github:helix-editor/helix";
-    # custom-flake.url = "github:user/repo";
   };
-
-  outputs = { self, nixpkgs, helix, ... }: {
-    pkgs = import nixpkgs {
+  outputs = { self, nixpkgs, helix, ... }: 
+    let
       system = "x86_64-linux";
-      config = {
-        allowUnfree = true;
+      pkgs = import nixpkgs {
+        inherit system;
+        config = {
+          allowUnfree = true;
+        };
+        overlays = [
+          (final: prev: {
+            helix = builtins.trace "Evaluating Helix overlay" (
+              builtins.trace "Helix version: ${helix.packages.${system}.default.version}"
+              helix.packages.${system}.default
+            );
+          })
+        ];
       };
-      overlays = [
-        (final: prev: {
-          # Example overlay to add a custom package
-          # customPackage = custom-flake.packages.x86_64-linux.customPackage;
-          # helix = helix.packages.x86_64-linux.helix;
-          # helix = helix.packages.${prev.system}.default;
-          helix = builtins.trace "Evaluating Helix overlay" (
-            builtins.trace "Helix version: ${helix.packages.${system}.default.version}"
-            helix.packages.${system}.default
-          );
-
-        })
-      ];
+    in {
+      pkgs = builtins.trace "Returning pkgs from pkgs/flake.nix" pkgs;
+      lib = nixpkgs.lib;
     };
-
-    lib = nixpkgs.lib;
-  };
 }
