3d2c9a6aa7fbb023fdc0412a1aeca076b8d970c0
Kyle Petryszak <6314611+ProjectInitiative@users.noreply.github.com>
2025-02-17 05:07:52 +0000

    more testing

--- modules/nixos/hosts/base-vm/default.nix
+++ modules/nixos/hosts/base-vm/default.nix
@@ -30,62 +30,62 @@ in
       device = "/dev/disk/by-label/nixos";
       fsType = "ext4";
     };
-    disko.devices = {
-      disk = {
-        one = {
-          type = "disk";
-          device = "/dev/vda";
-          content = {
-            type = "gpt";
-            partitions = {
-              boot = {
-                size = "500M";
-                type = "EF00";
-                content = {
-                  type = "filesystem";
-                  format = "vfat";
-                  mountpoint = "/boot";
-                  mountOptions = [ "umask=0077" ];
-                };
-              };
-              primary = {
-                size = "100%";
-                content = {
-                  type = "lvm_pv";
-                  vg = "pool";
-                };
-              };
-            };
-          };
-        };
-      };
-      lvm_vg = {
-        pool = {
-          type = "lvm_vg";
-          lvs = {
-            root = {
-              size = "100%FREE";
-              content = {
-                type = "filesystem";
-                format = "ext4";
-                mountpoint = "/";
-                mountOptions = [
-                  "defaults"
-                ];
-              };
-            };
-            # home = {
-            #   size = "100%FREE";
-            #   content = {
-            #     type = "filesystem";
-            #     format = "ext4";
-            #     mountpoint = "/home";
-            #   };
-            # };
-          };
-        };
-      };
-    };
+    # disko.devices = {
+    #   disk = {
+    #     one = {
+    #       type = "disk";
+    #       device = "/dev/vda";
+    #       content = {
+    #         type = "gpt";
+    #         partitions = {
+    #           boot = {
+    #             size = "500M";
+    #             type = "EF00";
+    #             content = {
+    #               type = "filesystem";
+    #               format = "vfat";
+    #               mountpoint = "/boot";
+    #               mountOptions = [ "umask=0077" ];
+    #             };
+    #           };
+    #           primary = {
+    #             size = "100%";
+    #             content = {
+    #               type = "lvm_pv";
+    #               vg = "pool";
+    #             };
+    #           };
+    #         };
+    #       };
+    #     };
+    #   };
+    #   lvm_vg = {
+    #     pool = {
+    #       type = "lvm_vg";
+    #       lvs = {
+    #         root = {
+    #           size = "100%FREE";
+    #           content = {
+    #             type = "filesystem";
+    #             format = "ext4";
+    #             mountpoint = "/";
+    #             mountOptions = [
+    #               "defaults"
+    #             ];
+    #           };
+    #         };
+    #         # home = {
+    #         #   size = "100%FREE";
+    #         #   content = {
+    #         #     type = "filesystem";
+    #         #     format = "ext4";
+    #         #     mountpoint = "/home";
+    #         #   };
+    #         # };
+    #       };
+    #     };
+    #   };
+    # };
 
 
     services.qemuGuest.enable = true;
