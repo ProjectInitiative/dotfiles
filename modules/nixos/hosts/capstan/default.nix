6a530d550b7c40e8cc741aeecd40f3efd9b4457d
Kyle Petryszak <6314611+ProjectInitiative@users.noreply.github.com>
2025-02-27 00:22:26 -0600

    re-enabling k8s

--- modules/nixos/hosts/capstan/default.nix
+++ modules/nixos/hosts/capstan/default.nix
@@ -90,7 +90,7 @@ in
 
       services = {
         k8s = {
-          enable = false;
+          enable = true;
           tokenFile = sops.secrets.k8s_token.path;
           role = "server";
           extraArgs = [
