52903049be080f05d7c9674925a74908aa03781d
Kyle Petryszak <6314611+ProjectInitiative@users.noreply.github.com>
2025-03-22 16:16:13 -0500

    adding tailscale to base router

--- systems/aarch64-sd-aarch64/stormjib/default.nix
+++ systems/aarch64-sd-aarch64/stormjib/default.nix
@@ -3,11 +3,33 @@
 #     Topsail: Agile sail for fair-weather speed (primary performance).
 #     StormJib: Rugged sail for heavy weather (backup resilience).
 
-{ ... }:
+{ pkgs, ... }:
 {
   config = {
     projectinitiative = {
-      hosts.masthead.stormjib.enable = true;
+      hosts.masthead.stormjib.enable = false;
+      networking = {
+        tailscale.enable = true;
+      };
     };
+
+    # Raspberry Pi specific settings
+    hardware.raspberry-pi."4".apply-overlays-dtmerge.enable = true;
+    hardware.deviceTree.enable = true;
+    
+    # Include essential Pi firmware
+    hardware.firmware = [ pkgs.raspberrypiWirelessFirmware ];
+    
+    # Boot settings for Raspberry Pi
+    boot = {
+      kernelPackages = pkgs.linuxPackages_rpi4;
+      initrd.availableKernelModules = [ "usbhid" "usb_storage" ];
+      loader = {
+        grub.enable = false;
+        generic-extlinux-compatible.enable = true;
+      };
+    };
+
+    services.openssh.enable = true;
   };
 }
