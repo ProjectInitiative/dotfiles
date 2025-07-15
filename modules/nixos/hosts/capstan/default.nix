ec0e14ff4bb382f142b9c0ebda836c92b1c9b56b
Kyle Petryszak <6314611+ProjectInitiative@users.noreply.github.com>
2025-07-15 15:17:40 -0500

    adding tpm tools

--- modules/nixos/hosts/capstan/default.nix
+++ modules/nixos/hosts/capstan/default.nix
@@ -121,14 +121,18 @@ in
     #   '';
 
     # };
+    #
+    # https://nixos.wiki/wiki/TPM
     # for openbao unsealing
     security = {
       tpm2 = {
         enable = true;
-        pkcs11.enable = true;
-        tctiEnvironment.enable = true;
+        pkcs11.enable = true; # expose /run/current-system/sw/lib/libtpm2_pkcs11.so
+        tctiEnvironment.enable = true; # TPM2TOOLS_TCTI and TPM2_PKCS11_TCTI env variables
       };
     };
+
+    # users.users.YOUR_USER.extraGroups = [ "tss" ];  # tss group has access to TPM devices
 
     environment.systemPackages = with pkgs; [
       bcachefs-tools
