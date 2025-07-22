e5cd220675ecc5825f5d0fa8eea82d243c2e9bee
Kyle Petryszak <6314611+ProjectInitiative@users.noreply.github.com>
2025-07-21 20:59:52 -0500

    adding bat aliases

--- modules/home/cli-apps/bat/default.nix
+++ modules/home/cli-apps/bat/default.nix
@@ -25,7 +25,20 @@ in
 
       shellAliases = {
         cat = "bat";
+        batlog = "bat --paging=never -l log";
       };
     };
+
+    programs.zsh.initContent = mkIf config.programs.zsh.enable ''
+      batdiff() {
+          git diff --name-only --relative --diff-filter=d -z | xargs --null bat --diff
+      }
+    '';
+
+    programs.bash.initExtra = mkIf config.programs.bash.enable ''
+      batdiff() {
+          git diff --name-only --relative --diff-filter=d -z | xargs --null bat --diff
+      }
+    '';
   };
 }
