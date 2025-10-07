8ba8a7ce14d942590e76a00b661d66bbb49a5163
Kyle Petryszak <6314611+ProjectInitiative@users.noreply.github.com>
2025-10-07 08:15:43 -0500

    adding qwen-cli to overlays

--- modules/common/suites/development/default.nix
+++ modules/common/suites/development/default.nix
@@ -81,8 +81,9 @@ in
           # ventoy-full - removed until https://github.com/ventoy/Ventoy/issues/3224 is resolved.
           rustup
           gemini-cli
+          qwen-code
           # pkgs.${namespace}.gemini-cli
-          pkgs.${namespace}.qwen-code
+          # pkgs.${namespace}.qwen-code
         ];
         # ++ lib.optionals isGraphical [ inputs.claude-desktop.packages.${system}.claude-desktop-with-fhs ];
       };
--- overlays/agentic-cli/default.nix
+++ overlays/agentic-cli/default.nix
@@ -1,5 +1,5 @@
 { channels, inputs, ... }:
 final: prev: {
   # Just use the version from the channels.nixpkgs that you've provided
-  inherit (channels.upstream) gemini-cli;
+  inherit (channels.upstream) gemini-cli qwen-code;
 }
