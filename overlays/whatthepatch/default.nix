e3fa11d899a6a48a3475b75fd275dbc55e643ee0
Kyle Petryszak <6314611+ProjectInitiative@users.noreply.github.com>
2026-06-02 17:00:45 +0000

    fix: override whatthepatch from upstream for both python3/312

--- overlays/whatthepatch/default.nix
+++ overlays/whatthepatch/default.nix
@@ -2,13 +2,11 @@
   python3Packages = prev.python3Packages.override {
     overrides = pyfinal: pyprev: {
       whatthepatch = channels.upstream.python3Packages.whatthepatch;
-      python-lsp-server = channels.upstream.python3Packages.python-lsp-server;
     };
   };
   python312Packages = prev.python312Packages.override {
     overrides = pyfinal: pyprev: {
       whatthepatch = channels.upstream.python312Packages.whatthepatch;
-      python-lsp-server = channels.upstream.python312Packages.python-lsp-server;
     };
   };
 }
